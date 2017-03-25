5262c107948dbbc7fc91b3cfb17d2ead81ff15fe
Tronix <rajdevkar.6@gmail.com>
2017-03-25 16:54:10 +0530

    Initial commit

---
+++ app/src/main/java/com/watchdroid/LockScreenService.java
@@ -0,0 +1,79 @@
+package com.watchdroid;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.arx_era.watchdroid.R;
+
+/**
+ * Created by Tronix on 25-03-2017.
+ */
+
+public class LockScreenService extends Service implements View.OnClickListener {
+
+
+    private LinearLayout linearLayout;
+    private WindowManager.LayoutParams layoutParams;
+    private WindowManager windowManager;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Not used
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(screenReceiver, intentFilter);
+        windowManager = ((WindowManager) getSystemService(WINDOW_SERVICE));
+        layoutParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN //draw on status bar
+                        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION,// hiding the home screen button
+                PixelFormat.TRANSLUCENT);
+    }
+
+    private void init() {
+        linearLayout = new LinearLayout(this);
+        windowManager.addView(linearLayout, layoutParams);
+        ((LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.lock_main, linearLayout);
+    }
+
+    @Override
+    public void onClick(View view) {
+        windowManager.removeView(linearLayout);
+        linearLayout = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(screenReceiver);
+        super.onDestroy();
+    }
+
+    BroadcastReceiver screenReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF) && linearLayout == null) {
+                init();
+            }
+        }
+    };
+
+
+}
